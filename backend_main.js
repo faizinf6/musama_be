b25575742194dad29d954c896f67b54a29613da8
KadzalikLab <faizinfaizinf6@gmail.com>
2024-04-02 21:12:47 +0700

    Initial commit

---
+++ backend_main.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from "cors";
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+import session from 'express-session'
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.use(session({
+    secret: 'rahasia', // Ganti dengan secret key Anda
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Untuk HTTPS gunakan `true`
+}));
+
+app.get('/', async (req, res) => {
+    res.json({ status: true, message: "Berjalan Coooy!!" })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
